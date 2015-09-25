a91c10bb530434ed9d0af10aab12d17d0d5165bf
Hans Hansen <h-hansen1990@hotmail.com>
2015-09-25 22:33:02 +0200

    Added Vector3.Lerp for the elevator instead of Vector3.MoveTowards.

--- Assets/Scripts/ElevatorScript.cs
+++ Assets/Scripts/ElevatorScript.cs
@@ -7,9 +7,12 @@ public class ElevatorScript : MonoBehaviour {
     public Transform end;
     public Transform[] nodes;
     public float speed;
+    private float startTime;
+ 
 	// Use this for initialization
 	void Start () {
-        speed = 2;
+        startTime = Time.time;
+        speed = 40;
 	}
 	
 	// Update is called once per frame
@@ -24,10 +27,11 @@ public class ElevatorScript : MonoBehaviour {
 
         if (isActivated)
         {
-            float step = speed * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, end.position, step);
+            //float step = speed * Time.deltaTime;
+            //transform.position = Vector3.MoveTowards(transform.position, end.position, step);
+            transform.position = Vector3.Lerp(transform.position, end.transform.position, (Time.time - startTime) / speed);
         }
-
+        
 	}
 
     void OnDrawGizmos()
