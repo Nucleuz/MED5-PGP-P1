90b2db2f2c19b4e7bc58d9308ed0f69a8d61fdd7
Hethix <An.ryge@gmail.com>
2015-09-25 15:00:40 +0200

    Getting started on GameManager/Trigger

---
+++ Assets/Scripts/Trigger/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+ 
+
+	public bool isLevelStarted = false;
+	public int numberOfCompletedTask = 0;
+	public bool isCurrentLevelCompleted = false;
+	public int numberOfCompletedLevels = 0;
+
+	public LevelManager LM;
+
+	public GameObject testCube;
+
+	// Use this for initialization
+	void Start () {
+		LM = GameObject.Find("LevelManagerObject").GetComponent<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void checkCompletion(GameObject[] events, int numberOfEvents){
+
+		//Receive data from each gameobject in booleans
+	}
+
+	/*void checkTaskCompletion(){
+			Not used at the moment
+	}*/
+
+	void changeLevel(){
+
+	}
+
+
+}
---
+++ Assets/Scripts/Trigger/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public int numberOfEvents;
+	public GameObject[] orderOfEvents;
+	public int[] eventOrder; 
+	//public int currentLevelNumber; //might not need this if only 1 scene
+
+	// Use this for initialization
+	void Start () {
+		countEventsInScene();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void countEventsInScene() { //Was made for recounting if there is several scenes
+		//Should count the number of events that is going to happen in each level
+		numberOfEvents = GameObject.FindGameObjectsWithTag("Test Tag").Length; //counts how many events should be triggered
+	}
+
+	void newLevel() {
+		numberOfEvents = 0;
+		//Should reset the values for how many events etc. there is in the level
+		//should call the method "countEventsInScene"
+	}
+}
