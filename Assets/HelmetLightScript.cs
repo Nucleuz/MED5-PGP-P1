cb51bac25530bdbbcc875c02c332562c1c09c22b
Nicolai Foldager <nfoldager0@gmail.com>
2015-09-25 12:16:45 +0200

    Helmet light works 50% now

---
+++ Assets/HelmetLightScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelmetLightScript : MonoBehaviour {
+
+    public float angleNormal = 45;                                                  //Angle of the spotlight without focus
+    public float angleFocus = 10;                                                   //Angle of the spotlight during focus
+    public float intensityNormal = 1;                                               //Intensity of the spotlight without focus
+    public float intensityFocus = 8;                                                //Intensity of the spotlight during focus
+
+    public Light helmetLight;                                                       //Object that refers to the spotlight within the scene
+
+    float startTime;
+    bool timeSaved = false;
+
+    void Start () {
+
+        helmetLight = GetComponent<Light>();                                        //Calls the light component on the spotlight
+
+        Debug.Log("AngleNorm: " + angleNormal);
+        Debug.Log("AngleFocus: " + angleFocus);
+
+    }
+	
+	void Update () {
+
+        if(Input.GetKey("space"))                                                   //Checks if the focus button is pressed (Default = space)
+        {
+
+            if(timeSaved == false)
+            {
+                startTime = Time.time;
+                timeSaved = true;
+            }
+            
+            float fadeTime = 1.0f;
+
+            Debug.Log("startTime: " + (Time.time - startTime) / fadeTime);
+
+            helmetLight.spotAngle = Mathf.Lerp(angleNormal, angleFocus, (Time.time - startTime)/ fadeTime);
+            helmetLight.intensity = Mathf.Lerp(intensityNormal, intensityFocus, (Time.time - startTime) / (fadeTime * 5));
+
+        } else
+        {
+            helmetLight.spotAngle = angleNormal;
+            helmetLight.intensity = intensityNormal;
+
+            timeSaved = false;
+        }
+	
+	}
+}
