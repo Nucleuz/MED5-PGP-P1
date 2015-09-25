9adbd691f2c1d9dec9b826d5cc00f08522692de1
pingo2 <ingemannofdoom@gmail.com>
2015-09-25 15:42:06 +0200

    Created cart script - Moves between rail points, rotates cart according to curve. Created rail script - allows for in editor rail changes. Rails change rotation according to neighbouring rails. Created red materials - red material for cart placeholder. Created material folder under assets.

---
+++ Assets/Scripts/Rail.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Rail : MonoBehaviour {
+
+	public Rail NextRail;
+	public Rail PreviousRail;
+
+	void Start(){
+
+	}
+
+	void Update(){
+		/*
+			Makes sure that application is not running.
+			This part is for level editing.
+		 */
+		if(!Application.isPlaying){
+			/*Checks for corner rails (if a previous or next rail is null
+			 it must be an end railpoint) and sets rotation according to neighbouring railpoints*/
+			if(NextRail != null && PreviousRail != null) {
+				transform.rotation = Quaternion.LookRotation(NextRail.transform.position - PreviousRail.transform.position);
+
+				/*sets rotation for first end point*/
+			} else if(PreviousRail == null) {
+				transform.LookAt(NextRail.transform);
+				/*sets rotation for second end point*/
+			} else if(NextRail == null) {
+				transform.rotation = Quaternion.LookRotation(transform.position - PreviousRail.transform.position);
+			}
+
+			/*Visual debugging (shows direction vector)*/
+			if(NextRail != null){
+				Debug.DrawLine(transform.position, NextRail.transform.position, Color.red);
+			}
+		}
+	}
+}
