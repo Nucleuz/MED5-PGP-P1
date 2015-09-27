29b4da24d5884b342c10dafcb5947e00c2de408e
Hethix <An.ryge@gmail.com>
2015-09-27 15:36:18 +0200

    ALOT of comments to the code, and some small changes

--- Assets/Scripts/Trigger/GameManager.cs
+++ Assets/Scripts/Trigger/GameManager.cs
@@ -4,39 +4,38 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
  
 
-	public bool isLevelStarted = false;
-	public int numberOfCompletedTask = 0;
-	public bool isCurrentLevelCompleted = false;
-	public int numberOfCompletedLevels = 0;
-	protected int numberOfTriggeredEvents = 0;
-	protected int index = 0;
+	public bool isLevelStarted = false; //IS NOT USED
+	public bool isCurrentLevelCompleted = false; //IS NOT USED
+	public int numberOfCompletedLevels = 0; //IS NOT USED - These are all made in case of several scenes!
+	protected int numberOfTriggeredEvents = 0; //Is used to determine where we should continue from each time a puzzle sequence is finished
+	protected int index = 0; // used to count which sequence is currently in play
 
-	public LevelManager LM;
-
-	public GameObject testCube;
+	public LevelManager LM; //Used in order to access the LevelManagerObject in the scene
 
 	// Use this for initialization
 	void Start () {
-		LM = GameObject.Find("LevelManagerObject").GetComponent<LevelManager>();
+		LM = GameObject.Find("LevelManagerObject").GetComponent<LevelManager>(); //accessing the LevelManager script on the LevelManagerObject
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if(index < LM.eventOrder.Length){
-			for(int j = numberOfTriggeredEvents; j < LM.eventOrder[index]; j++){
-				if(LM.events[j].GetComponent<Testingtrigger>().isTriggered == true){
-				numberOfTriggeredEvents++;
-				}
-				if(numberOfTriggeredEvents == LM.eventOrder[index]){
-					index++;
-					Debug.Log("Door is open! :D");
+		if(index < LM.eventOrder.Length){ //if the sequence is smaller then the amount of sequences then do the following. THIS MIGHT CAUSE TROUBLE IN THE END! Maybe delete?
+			for(int j = numberOfTriggeredEvents; j < LM.eventOrder[index]; j++){ //Runs the objects in the current sequence
+				if(LM.events[j].GetComponent<Testingtrigger>().isTriggered == true){ /*Checks if the current object is triggered - "isTriggered" is a boolean we made in order to test.
+				"isTriggered" should be replaced by another boolean which is needed on all the interactable objects.
+				There should be a if statement for each different interactable object, which fetches the correct script */
+				numberOfTriggeredEvents++; //count up if they are triggered
+				} 
+				if(numberOfTriggeredEvents == LM.eventOrder[index]){ //Checks if the right amount of events are triggered
+					index++; //Goes to the next sequence
+					Debug.Log("Door is open! :D"); //Should write code here for what is to happen with the next sequence of objects
 				}
 			}	
 		}
 	}
 
-	void checkCompletion(GameObject[] events, int numberOfEvents){
+	void checkCompletion(GameObject[] events, int numberOfEvents){ //IS NOT USED
 
 		//Receive data from each gameobject in booleans
 	}
@@ -45,7 +44,7 @@ public class GameManager : MonoBehaviour {
 			Not used at the moment
 	}*/
 
-	void changeLevel(){
+	void changeLevel(){ //was made in case of several scenes
 
 	}
 
--- Assets/Scripts/Trigger/LevelManager.cs
+++ Assets/Scripts/Trigger/LevelManager.cs
@@ -3,9 +3,9 @@ using System.Collections;
 
 public class LevelManager : MonoBehaviour {
 
-	public int numberOfEvents;
-	public GameObject[] events;
-	public int[] eventOrder; 
+	public int numberOfEvents; //IS NOT USED!!!
+	public GameObject[] events; // Events which are dragged into the levelmanager - Should be specified in the unity editor!!
+	public int[] eventOrder; //The event order in which you want things triggered - Should be specified in the unity editor!!
 	//public int currentLevelNumber; //might not need this if only 1 scene
 
 	// Use this for initialization
