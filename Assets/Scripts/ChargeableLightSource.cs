57fd490f1111479785cfab3c12faeea50873fef2
Nicolai Foldager <nfoldager0@gmail.com>
2015-09-28 21:37:27 +0200

    Can't progress before player colors are stored. Also, how to change color of the texture of a material we dont know

--- Assets/Scripts/ChargeableLightSource.cs
+++ Assets/Scripts/ChargeableLightSource.cs
@@ -6,10 +6,18 @@ public class ChargeableLightSource : MonoBehaviour {
     public Transform chargedBeamTargetPosition;
 
     public bool isActivated;
+    public float timeToFullyCharge;
 
     public ParticleSystem particles;
 
+    public Renderer render;
+
 	void Start () {
+
+        render = GetComponent<Renderer>();
+
+        timeToFullyCharge = 3.0f;
+
         particles = GetComponent<ParticleSystem>();
         particles.Pause();
     }
@@ -18,6 +26,10 @@ public class ChargeableLightSource : MonoBehaviour {
 
         if(isActivated) {
             particles.Play();
+            render.material.color = Color.green;
+        } else {
+            particles.Pause();                                                  //Gotta find a proper way to clear particles. This is not pretty
+            render.material.color = Color.grey;
         }
 	    
 	}
