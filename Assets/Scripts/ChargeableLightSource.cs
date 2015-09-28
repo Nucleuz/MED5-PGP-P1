5e2c4c5728b73e4a28499d3628f9ca26779577d0
Nicolai Foldager <nfoldager0@gmail.com>
2015-09-28 20:51:27 +0200

    Added particle system when it is triggered by the beam from the helmet

---
+++ Assets/Scripts/ChargeableLightSource.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChargeableLightSource : MonoBehaviour {
+
+    public Transform chargedBeamTargetPosition;
+
+    public bool isActivated;
+
+    public ParticleSystem particles;
+
+	void Start () {
+        particles = GetComponent<ParticleSystem>();
+        particles.Pause();
+    }
+	
+	void Update () {
+
+        if(isActivated) {
+            particles.Play();
+        }
+	    
+	}
+}
