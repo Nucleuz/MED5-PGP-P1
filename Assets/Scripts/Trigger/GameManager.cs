f7cb0ac6614c5cbc3a64cdb10dcf0f01d41e1757
Hethix <An.ryge@gmail.com>
2015-09-28 13:17:52 +0200

    Now with comments! ALOT of comments

--- Assets/Scripts/Trigger/GameManager.cs
+++ Assets/Scripts/Trigger/GameManager.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
  
 
-	protected int numberOfTriggeredEvents = 0; //Is used to determine where we should continue from each time a puzzle sequence is finished
-	protected int currentNumberOfEventsTriggered = 0;
+	protected int numberOfTriggeredEvents = 0; //Is used to determine where we should continue from each time a puzzle sequence is finished. is only updated after each sequence is finished
+	protected int currentNumberOfEventsTriggered = 0; //counts up when a single event is triggered, is reset when all events in the sequence is triggered
 	protected int index = 0; // used to count which sequence is currently in play
 
 	public LevelManager LM; //Used in order to access the LevelManagerObject in the scene
@@ -14,7 +14,7 @@ public class GameManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		LM = GameObject.Find("LevelManagerObject").GetComponent<LevelManager>(); //accessing the LevelManager script on the LevelManagerObject
-		for(int k = 0; k < LM.eventOrder[0]; k++){
+		for(int k = 0; k < LM.eventOrder[0]; k++){ //makes the first events in the scene triggerable
 			LM.events[k].GetComponent<Trigger>().isReadyToBeTriggered = true;
 		}
 	}
@@ -23,30 +23,24 @@ public class GameManager : MonoBehaviour {
 	void Update () {
 
 		for(int j = 0; j < LM.eventOrder[index]; j++){ //Runs the objects in the current sequence
-			//Debug.Log(j);
-			if(LM.events[j + numberOfTriggeredEvents].GetComponent<Trigger>().isTriggered == true && LM.events[j + numberOfTriggeredEvents].GetComponent<Trigger>().isReadyToBeTriggered == true){ //Checks if the current object is triggered
-				//numberOfTriggeredEvents++; //count up if they are triggered
-				//Debug.Log("Triggered events " + numberOfTriggeredEvents);
-				LM.events[j + numberOfTriggeredEvents].GetComponent<Trigger>().isReadyToBeTriggered = false;
-				currentNumberOfEventsTriggered++;
-				Debug.Log("CNOET " + currentNumberOfEventsTriggered);
+			//Checks if the current object is triggered, and if they are ready to be triggered
+			//Used in order to not get double values
+			if(LM.events[j + numberOfTriggeredEvents].GetComponent<Trigger>().isTriggered == true && LM.events[j + numberOfTriggeredEvents].GetComponent<Trigger>().isReadyToBeTriggered == true){ 
+				LM.events[j + numberOfTriggeredEvents].GetComponent<Trigger>().isReadyToBeTriggered = false; //sets the event untriggerable
+				currentNumberOfEventsTriggered++; //counts up the events in sequence by 1
 			} 
 		}
-		if(currentNumberOfEventsTriggered == LM.eventOrder[index]){ //Checks if the right amount of events are triggered
-			if(index < LM.eventOrder.Length - 1){
-				Debug.Log("CNOET2" + currentNumberOfEventsTriggered);
-				Debug.Log("event order" + " " + index);
-				numberOfTriggeredEvents += LM.eventOrder[index];
+
+		if(currentNumberOfEventsTriggered == LM.eventOrder[index]){ //Checks if the right amount of events are triggered in the current sequence
+			if(index < LM.eventOrder.Length - 1){ //Checks if it is the last sequence of events - if it is: skip this
+				numberOfTriggeredEvents += LM.eventOrder[index]; //Increase the total number of events by the amount of events that was in the current sequence
 				index++; //Goes to the next sequence
-				for(int i = numberOfTriggeredEvents; i < numberOfTriggeredEvents + LM.eventOrder[index]; ++i){
-					//Debug.Log("i " + i);
-					LM.events[i].GetComponent<Trigger>().isReadyToBeTriggered = true;
-					//Debug.Log("event order" + " " + index);
-					Debug.Log("This is now ready");
+				for(int i = numberOfTriggeredEvents; i < numberOfTriggeredEvents + LM.eventOrder[index]; ++i){ //Goes through the next sequence of events
+					LM.events[i].GetComponent<Trigger>().isReadyToBeTriggered = true; //Makes the next sequence ready to be triggered 
 				}
 			}
 			Debug.Log("Door is open! :D"); //Should write code here for what is to happen with the next sequence of objects
-			currentNumberOfEventsTriggered = 0;
+			currentNumberOfEventsTriggered = 0; //Resets the amount of objects that was triggered in the current sequence
 		}	
 	}
 }
--- Assets/Scripts/Trigger/Testingtrigger.cs
+++ Assets/Scripts/Trigger/Testingtrigger.cs
@@ -5,9 +5,6 @@ public class Trigger : MonoBehaviour {
 
 	public bool isTriggered = false;
 	public bool isReadyToBeTriggered = false;
-	//color
-	Color ourColor = new Color(0,255,0,1);
-	Color triggeredColor = new Color(255,0,0,1);
 	// Use this for initialization
 	void Start () {
 	
@@ -16,15 +13,12 @@ public class Trigger : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(isReadyToBeTriggered == true){
-
 		}
 	}
 
 	void OnTriggerEnter(Collider trigger){
 		if(isReadyToBeTriggered == true){
 			isTriggered = true;
-			Debug.Log("Cube is touched");
-
 		}
 		
 	}
