a8df6ab3a6c2f1b6c20387b6e3434e9eaaa03191
Nicolai Foldager <nfoldager0@gmail.com>
2015-09-28 11:42:48 +0200

    Elevator now works between multiple points. Level editor have to place empty game objects in the scene and assign them to the nodes array.
    Still missing:
     * What activates the elevator (Currently it's a mouse press)
     * Disable the calculation of time used in lerp when last node is visited
     * Set player as child of elevator to move them along

--- Assets/Scripts/ElevatorScript.cs
+++ Assets/Scripts/ElevatorScript.cs
@@ -2,17 +2,19 @@
 using System.Collections;
 
 public class ElevatorScript : MonoBehaviour {
-    public bool isActivated = false;
 
-    public Transform end;
-    public Transform[] nodes;
-    public float speed;
-    private float startTime;
+    public bool isActivated = false;                                        //Check to see if the object has been activated
+
+    public Transform[] nodes;                                               //Array of transform objects that holds the positions the elevator will visit
+    private int activeNode;                                                 //Int that counts the number of nodes that has been visited
+    public float speed;                                                     //Speed of the elevator
+
+    private float currentLerpTime;                                          //holds the amount of time the lerp has been running
  
 	// Use this for initialization
 	void Start () {
-        startTime = Time.time;
-        speed = 40;
+        speed = 1000;                                                       //Sets the speed of the elevator
+        activeNode = 0;                                                     //Sets the visited nodes to zero
 	}
 	
 	// Update is called once per frame
@@ -22,21 +24,43 @@ public class ElevatorScript : MonoBehaviour {
         if (Input.GetMouseButtonDown(0))
         {
             isActivated = true;
-            Debug.Log("Mouse pressed!");
         }
 
-        if (isActivated)
+        if (isActivated)                                                    //Activates after a mousepress
         {
-            //float step = speed * Time.deltaTime;
-            //transform.position = Vector3.MoveTowards(transform.position, end.position, step);
-            transform.position = Vector3.Lerp(transform.position, end.transform.position, (Time.time - startTime) / speed);
+            float lerpTime = 1.0f;                                          //Sets the time to lerp the elevator between two points.
+
+            currentLerpTime += Time.deltaTime;                              //Adds deltatime to the time to lerp
+
+            //Makes sure that the time to lerp never exceeds float lerpTime
+            if (currentLerpTime > lerpTime) {
+                currentLerpTime = lerpTime;
+            }
+
+            float t = currentLerpTime / lerpTime * 0.15f;                   //Calculates the final speed of the object.
+
+            Debug.Log(t);
+            
+            //Moves the elevator from it's current position to the next active node
+            transform.position = Vector3.MoveTowards(transform.position, nodes[activeNode].position, t);
+
+            //Checks if the distance between the elevator and current active node is less than 0.1 and if active node is not larger than array nodes length.
+            if (Vector3.Distance(transform.position, nodes[activeNode].transform.position) < 0.1f && activeNode < nodes.Length - 1) {
+                activeNode++;                                               //Adds 1 to the active node in order to move on
+            }
         }
         
 	}
 
     void OnDrawGizmos()
     {
-        Gizmos.color = Color.magenta;
-        Gizmos.DrawWireCube(end.position, new Vector3(0.5f, 0.5f, 0.5f));
+        Gizmos.color = Color.magenta;                                       //Draws the gizmos magenta
+     
+        for (int i = 0; i < nodes.Length; i++)
+            Gizmos.DrawWireCube(nodes[i].position, new Vector3(0.5f, 0.5f, 0.5f));  //Draws a cube at each node
+
+        for (int i = 1; i < nodes.Length; i++)
+            Gizmos.DrawLine(nodes[i - 1].position, nodes[i].position);              //Draws a line between the cubes in the order that they are visited.
+        
     }
 }
