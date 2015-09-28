5efcd4c1653539abaaaafe2609a4480180052d7b
Hans Hansen <h-hansen1990@hotmail.com>
2015-09-28 14:27:47 +0200

    Added SoundGemScript - Detects mouseclick atm (change this later)

---
+++ Assets/Scripts/SoundGemScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundGemScript : MonoBehaviour
+{
+    // Initialize an audio object
+    AudioSource audio;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Set audio to the AudioSource component
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This just detects the left mouse click atm, but we will eventually need something else to activate it.
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Sets the isTrigger variable in the trigger script to true.
+            GetComponent<Trigger>().isTriggered = true;
+        }
+
+        // Check if SoundGem is triggered
+        if (GetComponent<Trigger>().isTriggered)
+        {
+            // Play audio if SoundGem is triggered
+            audio.Play();
+            
+            // Set isTriggered to false so that we can keep activating it again.
+            GetComponent<Trigger>().isTriggered = false;
+        }
+    }
+}
