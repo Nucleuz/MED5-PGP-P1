18a4278745c85a814987010bebcd3faff69aea94
Hethix <An.ryge@gmail.com>
2015-09-30 15:47:28 +0200

    Mirrow rotation.

--- Assets/Scripts/Interactables/Mirror.cs
+++ Assets/Scripts/Interactables/Mirror.cs
@@ -4,7 +4,28 @@ using System.Collections;
 public class Mirror : Interactable {
 	[HideInInspector]
 	public GameObject triggeredPlayer;
+	public Trigger tr;
+	public float startPoint;
+	public float endPoint;
 
+
+	void Start(){
+		//transform.rotation.y = startPoint;
+	}
+	void Update(){
+		if(tr.isTriggered==true){
+			//Check if the rotation is less then or bigger then the goal rotation
+			//Rotate forth
+			if(transform.rotation.y < endPoint){
+				gameObject.transform.Rotate(0* Time.deltaTime,50* Time.deltaTime,0 * Time.deltaTime, Space.World);
+				//transform.rotation = Quaternion.Euler(startPoint, endPoint, 5*Time.deltaTime);
+			}
+			//Rotate back
+			else if(transform.rotation.y == endPoint){
+				transform.rotation = Quaternion.Euler(endPoint, startPoint, 5*Time.deltaTime);
+			}
+		}
+	}
 	public override void OnRayReceived(int playerIndex, Ray ray, RaycastHit hit, ref LineRenderer lineRenderer,int nextLineVertex){
 
 		Ray newRay = new Ray (hit.point, Vector3.Reflect (ray.direction, hit.normal));
