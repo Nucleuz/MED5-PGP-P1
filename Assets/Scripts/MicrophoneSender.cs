0527f22bab2ae7bfb6cbe395281b3162143558c4
Frans Peter Larsen <larsen.frans@gmail.com>
2015-09-30 13:04:25 +0200

    Basic Microphone Test

--- Assets/Scripts/MicrophoneSender.cs
+++ Assets/Scripts/MicrophoneSender.cs
@@ -5,36 +5,28 @@ using VoiceChat.Networking;
 using VoiceChat.Networking.Legacy;
 
 public class MicrophoneSender : MonoBehaviour {
-	public VoiceChatPlayer player;
-	public VoiceChatRecorder recorder;
-	public VoiceChatSettings settings;
+	private VoiceChatRecorder recorder;
 
 	// Use this for initialization
-	void Start () {	
-		Debug.Log ("Player: " + player.isActiveAndEnabled);
-		Debug.Log ("Recorder: " + recorder.isActiveAndEnabled);
-		Debug.Log ("Settings: " + settings.isActiveAndEnabled + " " + recorder.HasDefaultDevice + " " + recorder.HasSpecificDevice);
+	void Start () {
+		// Get Components for Pointers
+		recorder = GetComponent<VoiceChatRecorder> ();
 
-		recorder.NetworkId = -1;
+		// Presumes that the first device availiable is the best
+		recorder.Device = recorder.AvailableDevices [0];
+
+		//TODO: Get NetworkId from DarkRift before recording 
+		recorder.StartRecording();
+
+		//TODO: LIST
+		// 1. Set VoiceChatRecorder.Instance.NetworkId to a unique number on each client
+		// 2. Subscribe to the VoiceChatRecorder.Instance.NewSample event
+		// 3. When the NewSample events get triggered, send the VoiceChatPacket data to all other clients.
+		// 4. On the client read the data back, and make sure you have one instance of the Network/Resources/VoiceChat_Player prefab, grab the VoiceChatPlayer script and call OnNewSample(packet) on it.
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (recorder.PushToTalkKey) && !recorder.IsRecording)
-			recorder.StartRecording ();
-		if(Input.GetKeyUp(recorder.PushToTalkKey))
-			recorder.StopRecording();
 
-		if (recorder.IsRecording)
-			Debug.Log ("Recording");
-	}
-
-	void PrepareAudio() {
-		AudioClip clip = new AudioClip();
-
-		float[] samples = new float[clip.samples * clip.channels];
-		clip.GetData (samples, 0);
-		
-		VoiceChatPacket package = VoiceChatUtils.Compress (samples);
 	}
 }
