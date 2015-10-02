b02a822daf055169181a577d2ffcf6a82da839b8
Frans Peter Larsen <larsen.frans@gmail.com>
2015-10-02 14:20:20 +0200

    Added new Subject

--- Assets/Scripts/Networking/Network.cs
+++ Assets/Scripts/Networking/Network.cs
@@ -2,7 +2,7 @@
 By KasperHdL
 used to categories network messages
 
-//@NOTE: cannot use enums because that would require non-stop casting to byte and ushort
+//@NOTE: cannot use enums because that would require non stop casting to byte and ushort
 */
 
 public class Network{
@@ -12,23 +12,16 @@ public class Network{
 	public class Tag{
 		public const byte Manager 	= 1;
 		public const byte Player 	= 2;
-        public const byte Trigger   = 3;
 	}
 
-    //@TODO - clean this up
 	//what is the message about
 	public class Subject{
-		public const ushort HasJoined          = 1;
-		public const ushort SpawnPlayer        = 2;
-		public const ushort ServerSentSpawnPos = 3;
-		public const ushort ServerSentNetID    = 4;
-		public const ushort PlayerUpdate       = 5;
-        public const ushort TriggerActivate    = 6;
-        public const ushort TriggerDeactivate  = 7;
-        
-        public const ushort ServerLoadedLevel = 8;
-        public const ushort ServerSentTriggerIDs = 9;
-        public const ushort RequestTriggerIDs = 10;
+		public const ushort HasJoined 			= 1;
+		public const ushort SpawnPlayer 		= 2;
+		public const ushort ServerSentSpawnPos 	= 3;
+		public const ushort ServerSentNetID 	= 4;
+		public const ushort PlayerUpdate 		= 5;
+		public const ushort VoicePackage		= 6;
 	}
 
 
