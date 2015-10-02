7b567a177f94af9172ecc8c81b805f9ccba9d6a2
Itai Yavin <Itai.yavin@gmail.com>
2015-10-02 12:35:58 +0200

    Fixed elevator script, now elevator can go up AND down Fixed rail cart script so rail cart will follow elevator when on it. Added rail connection component: - Rail connection functions as a connection point between two rail segments - Rail connection can disconnect and connect to two individual points - Rail connection is designed to work with elevator as well as doors and other obstructions. - To work rail connection needs its next and previous rail points.
    Created elevator prefab.

--- Assets/Scripts/Interactables/ElevatorScript.cs
+++ Assets/Scripts/Interactables/ElevatorScript.cs
@@ -3,7 +3,11 @@ using System.Collections;
 
 public class ElevatorScript : MonoBehaviour {
 
+    public RailConnection rC;                                               //rC = railConnection
+    public Rail rP;                                                         //rP = railPoint
+
     public bool isActivated = false;                                        //Check to see if the object has been activated
+    public bool goingUp;
 
     public Transform[] nodes;                                               //Array of transform objects that holds the positions the elevator will visit
     private int activeNode;                                                 //Int that counts the number of nodes that has been visited
@@ -13,6 +17,13 @@ public class ElevatorScript : MonoBehaviour {
  
 	// Use this for initialization
 	void Start () {
+        goingUp = true;
+
+        rC = GetComponent<RailConnection>();
+        rP = GetComponent<Rail>();
+
+        rC.connectToPrev = true;
+
         speed = 1000;                                                       //Sets the speed of the elevator
         activeNode = 0;                                                     //Sets the visited nodes to zero
 	}
@@ -20,7 +31,7 @@ public class ElevatorScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        // Just checks if the mouse button is pressed
+        // Checks if the mouse button is pressed
         if (Input.GetMouseButtonDown(0))
         {
             isActivated = true;
@@ -28,6 +39,7 @@ public class ElevatorScript : MonoBehaviour {
 
         if (isActivated)                                                    //Activates after a mousepress
         {
+
             float lerpTime = 1.0f;                                          //Sets the time to lerp the elevator between two points.
 
             currentLerpTime += Time.deltaTime;                              //Adds deltatime to the time to lerp
@@ -38,16 +50,47 @@ public class ElevatorScript : MonoBehaviour {
             }
 
             float t = currentLerpTime / lerpTime * 0.15f;                   //Calculates the final speed of the object.
-
-            Debug.Log(t);
             
             //Moves the elevator from it's current position to the next active node
             transform.position = Vector3.MoveTowards(transform.position, nodes[activeNode].position, t);
-
+            
+            
             //Checks if the distance between the elevator and current active node is less than 0.1 and if active node is not larger than array nodes length.
-            if (Vector3.Distance(transform.position, nodes[activeNode].transform.position) < 0.1f && activeNode < nodes.Length - 1) {
-                activeNode++;                                               //Adds 1 to the active node in order to move on
+            if (Vector3.Distance(transform.position, nodes[activeNode].transform.position) < 0.1f && activeNode <= nodes.Length - 1) {
+                    activeNode++;
             }
+
+            // Checks if we've reached the last node. If true, then we reverse the order of the nodes, set the active node to 0, and deactivate the elevator. 
+            // It then goes backwards when we activate the elevator again.
+            if(Vector3.Distance(transform.position, nodes[nodes.Length-1].transform.position) < 0.1f){
+                System.Array.Reverse(nodes);
+                activeNode = 0;
+                if(goingUp){
+                    goingUp = false;
+                } else{
+                    goingUp = true;
+                }
+                isActivated = false;
+            } 
+
+            // Disconnects when the active nodes is not the first or last one
+            if(activeNode < nodes.Length-1 && activeNode > 0){
+                rC.connectToNext = false;
+                rC.connectToPrev = false;
+            }
+
+            // Checks whether the elevator has reached the last node in order to connect/disconnect to the rails.
+            if(activeNode == nodes.Length-1 && Vector3.Distance(transform.position, nodes[nodes.Length-1].transform.position) < 0.3f){
+                    if(goingUp){ //When end points are reached connection and disconnection to next and previous rail points is handled.
+                        rC.connectToPrev = false;
+                        rC.connectToNext = true;
+                    }
+                    if(!goingUp){
+                        rC.connectToNext = false;
+                        rC.connectToPrev = true;
+                    }
+            }
+
         }
         
 	}
