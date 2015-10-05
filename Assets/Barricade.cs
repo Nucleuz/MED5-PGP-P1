e158531a80ee8ac0e282d6f3b14750037d3b99e8
Hans Hansen <h-hansen1990@hotmail.com>
2015-10-05 15:38:17 +0200

    Added a Barricade script

---
+++ Assets/Barricade.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barricade : MonoBehaviour {
+
+    public RailConnection rC;
+    public bool isTriggered = false;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
