c3142d6c38f70bdc964c9840095e7558a3c460ca
Nicolai Foldager <nfoldager0@gmail.com>
2015-10-08 11:01:30 +0200

    Added teleport to cart command and a getter function for String messageClient

--- Assets/Scripts/Console/ClientCommands.cs
+++ Assets/Scripts/Console/ClientCommands.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class ClientCommands : MonoBehaviour {
+
 	//This should contain all the client methods.
 	public string messageClient;
 
@@ -14,6 +15,22 @@ public class ClientCommands : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    public string getMessageClient() {
+        return messageClient;
+    }
+
+    public string TeleportToCart() {
+        GameObject.Find("Player").transform.position = GameObject.Find("Cart").transform.position;
+
+        if(GameObject.Find("Player").transform.position != GameObject.Find("Cart").transform.position)
+            messageClient = "Teleport " + GameObject.Find("Player") + " to " + GameObject.Find("Cart") + " failed!";
+        else
+            messageClient = "Teleport " + GameObject.Find("Player") + " to " + GameObject.Find("Cart") + " succeeded!";
+
+        return messageClient;
+    }
+
 	public string GetPosition(){
 		messageClient = "xPos: " + GameObject.Find("Player").transform.position.x + " yPos: " + GameObject.Find("Player").transform.position.y + " zPos: " + GameObject.Find("Player").transform.position.z;
 		return messageClient;
