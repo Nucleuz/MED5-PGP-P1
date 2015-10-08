f4161c061a568fd8e23c90bee427004cb5406e31
Nicolai Foldager <nfoldager0@gmail.com>
2015-10-08 11:19:04 +0200

    Added comments to each function

--- Assets/Scripts/Console/ClientCommands.cs
+++ Assets/Scripts/Console/ClientCommands.cs
@@ -16,21 +16,24 @@ public class ClientCommands : MonoBehaviour {
 	
 	}
 
+    // Getter to the console script to call for a return message.
     public string getMessageClient() {
         return messageClient;
     }
 
+    // Teleports the player to their cart
     public string TeleportToCart() {
         GameObject.Find("Player").transform.position = GameObject.Find("Cart").transform.position;
 
-        if(GameObject.Find("Player").transform.position != GameObject.Find("Cart").transform.position)
-            messageClient = "Teleport " + GameObject.Find("Player") + " to " + GameObject.Find("Cart") + " failed!";
+        if(GameObject.Find("Player").transform.position == GameObject.Find("Cart").transform.position)
+            messageClient = "Teleported player to cart succeeded";
         else
-            messageClient = "Teleport " + GameObject.Find("Player") + " to " + GameObject.Find("Cart") + " succeeded!";
+            messageClient = "Teleported player to cart failed!";
 
         return messageClient;
     }
 
+    // Returns the position of the player
 	public string GetPosition(){
 		messageClient = "xPos: " + GameObject.Find("Player").transform.position.x + " yPos: " + GameObject.Find("Player").transform.position.y + " zPos: " + GameObject.Find("Player").transform.position.z;
 		return messageClient;
