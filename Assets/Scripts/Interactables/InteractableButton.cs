8f849dd63c3647212972507986c2d4da9c12fbc6
Maja Daniela Hansen <MajaDanielaHansen@Gmail.com>
2015-10-14 12:59:02 +0200

    Created individual buttons scriptwise which changes the color to be the color based on which player needs to trigger it. Implemented animation on activation, and sound. Also replaced the prefab for the button

--- Assets/Scripts/Interactables/InteractableButton.cs
+++ Assets/Scripts/Interactables/InteractableButton.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class InteractableButton : Interactable{
 	Trigger trigger;
+	Animator buttonAnimator;
+	Light buttonLight;
+	public AudioClip activatedButtonSound;
+	AudioSource audio;
+	//checks if the sound has been player, inorder to only play the sound only one time.
+	private bool isSoundPlayed = false;
 
 	[Tooltip("-1 = anyone, 1 = player1, 2 = player2, 4 = player3")]
 	public int playerID;
@@ -14,9 +20,43 @@ public class InteractableButton : Interactable{
 
 	// Use this for initialization
 	void Start () {
+		buttonAnimator = GetComponent<Animator>();
 		additionCheck = new bool[4];
 		delay = 1.0f;
 		trigger = GetComponent<Trigger> ();
+		buttonLight = GetComponent<Light>();
+		audio = GetComponent<AudioSource>();
+
+		//Set the color of the interactable button to the correct user
+		switch (playerID){
+			case -1: //anybody
+				buttonLight.color = Color.white;
+			break;
+			case 1:
+				buttonLight.color = Color.red;
+			break;
+			case 2:
+				buttonLight.color = Color.blue;
+			break;
+			case 3:
+				buttonLight.color = Color.magenta;
+			break;
+			case 4:
+				buttonLight.color = Color.green;
+			break;
+			case 5:
+				buttonLight.color = Color.yellow;
+			break;
+			case 6:
+				buttonLight.color = Color.cyan;
+			break;
+			case 7:
+				buttonLight.color = Color.white;
+			break;
+			default:
+                Debug.Log("Invalid playerIndex");
+            break;
+            }	
 	}
 	
 	// Update is called once per frame
@@ -28,8 +68,22 @@ public class InteractableButton : Interactable{
 			for(int i = 0; i < additionCheck.Length; i++)
 				additionCheck[i] = false;
 		}
+		if(trigger.isTriggered){
+			buttonAnimator.SetBool("isActivated", true);
+			playSound();
+		} else {
+			 
+			 buttonAnimator.SetBool("isActivated", false);
+			 isSoundPlayed = false;
+		}
 	}
 
+	void playSound(){
+		if(isSoundPlayed == false){ //if the sound has not been played.
+			audio.PlayOneShot(activatedButtonSound, 1F); // play sound with the volume of 1.
+			isSoundPlayed = true; // set to true, so the sound wont play twice.
+		}
+	}
 
 	public override void OnRayReceived(int playerIndex, Ray ray, RaycastHit hit, ref LineRenderer lineRenderer,int nextLineVertex){
 		if(!additionCheck[playerIndex-1]){
@@ -41,5 +95,4 @@ public class InteractableButton : Interactable{
 			trigger.isTriggered = true;
 		}
 	}
-
 }
