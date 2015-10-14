cee1d34be032c3eb3bf5004f48a8913ce2173eff
Itai Yavin <Itai.yavin@gmail.com>
2015-10-14 16:00:00 +0200

    Updated interactablebutton to be a comparison of two arrays instead of an integer addition.
    - Still needs to update the color check of button lights

--- Assets/Scripts/Interactables/InteractableButton.cs
+++ Assets/Scripts/Interactables/InteractableButton.cs
@@ -2,35 +2,40 @@
 using System.Collections;
 
 public class InteractableButton : Interactable{
-	Trigger trigger;
 	Animator buttonAnimator;
 	Light buttonLight;
+	ParticleSystem par;
+	
 	public AudioClip activatedButtonSound;
 	AudioSource audio;
-	ParticleSystem par;
 	//checks if the sound has been player, inorder to only play the sound only one time.
-	private bool isSoundPlayed = false;
 
-	[Tooltip("-1 = anyone, 1 = player1, 2 = player2, 4 = player3")]
-	public int playerID;
-	private int combinedPlayerID;
-	private bool[] additionCheck;
+	[Tooltip("Check the players that have to hit the button")]
+	public bool[] playerList;
+	
+	private bool[] playerCheck = new bool[3];
+	
+	private bool arraysFit;
+	private bool isSoundPlayed = false;
 
 	private float delay;
 	private float timer;
 
+	private Trigger trigger;
+	
 	// Use this for initialization
 	void Start () {
-		buttonAnimator = GetComponent<Animator>();
-		additionCheck = new bool[4];
-		delay = 1.0f;
-		trigger = GetComponent<Trigger>();
-		buttonLight = GetComponent<Light>();
-		audio = GetComponent<AudioSource>();
-		par = GetComponent<ParticleSystem>();
+		arraysFit 		= false;
+		delay 			= 1.0f;
+	
+		buttonAnimator 	= GetComponent<Animator>();
+		buttonLight 	= GetComponent<Light>();
+		trigger 		= GetComponent<Trigger>();
+		audio 			= GetComponent<AudioSource>();
+		par 			= GetComponent<ParticleSystem>();
 
 		//Set the color of the interactable button both background light and particles to the correct user.
-		switch (playerID){
+		/*switch (playerID){
 			case -1:
 				buttonLight.color = Color.white;
 				par.startColor = Color.white;
@@ -66,44 +71,56 @@ public class InteractableButton : Interactable{
 			default:
                 Debug.Log("Invalid playerIndex");
             break;
-            }	
+            }*/	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		timer -= Time.deltaTime;
-		if(timer <= 0){
-			timer = delay;
-			combinedPlayerID = 0;
-			for(int i = 0; i < additionCheck.Length; i++)
-				additionCheck[i] = false;
+
+		//This essentially updates the playerCheck arrays, so if one player stops hitting the button he will be removed
+		timer -= Time.deltaTime;							//CountDown
+		if(timer <= 0){										//Check if timer is up
+			timer = delay;									//Reset Timer
+			for(int i = 0; i < playerCheck.Length; i++){	//Reset playerCheck array
+				playerCheck[i] = false;
+			}
 		}
+
 		if(trigger.isTriggered){
-			buttonAnimator.SetBool("isActivated", true); //starts the animation of the button.
-			par.Play(); //starts the particles system.
-			playSound(); //starts the method which plays the sound.
+			buttonAnimator.SetBool("isActivated", true); 	//starts the animation of the button.
+			par.Play(); 									//starts the particles system.
+			playSound(); 									//starts the method which plays the sound.
 		} else {
-			 buttonAnimator.SetBool("isActivated", false); //stops the animation of the button.
-			 isSoundPlayed = false; //makes sure that the sound only plays once.
-			 par.Stop(); //stops the particle system.
+			 buttonAnimator.SetBool("isActivated", false); 	//stops the animation of the button.
+			 isSoundPlayed = false; 						//makes sure that the sound only plays once.
+			 par.Stop(); 									//stops the particle system.
 		}
 	}
 
 	void playSound(){
-		if(isSoundPlayed == false){ //if the sound has not been played.
-			audio.PlayOneShot(activatedButtonSound, 1F); // play sound with the volume of 1.
-			isSoundPlayed = true; // set to true, so the sound wont play twice.
+		if(isSoundPlayed == false){ 						//if the sound has not been played.
+			audio.PlayOneShot(activatedButtonSound, 1F); 	// play sound with the volume of 1.
+			isSoundPlayed = true; 							// set to true, so the sound wont play twice.
 		}
 	}
 
 	public override void OnRayReceived(int playerIndex, Ray ray, RaycastHit hit, ref LineRenderer lineRenderer,int nextLineVertex){
-		if(!additionCheck[playerIndex-1]){
-			combinedPlayerID += playerIndex;
-			additionCheck[playerIndex-1] = true;
+		if(!playerCheck[playerIndex-1]){
+			playerCheck[playerIndex-1] = true;
 		}
-		
-		if (trigger.isReadyToBeTriggered && (playerIndex == playerID || playerID == -1 || combinedPlayerID == playerID)) {
+
+		arraysFit = checkArrays(playerCheck, playerList);
+
+		if (trigger.isReadyToBeTriggered && arraysFit)
 			trigger.isTriggered = true;
+		
+	}
+
+	public bool checkArrays(bool[] a, bool[] b){
+		for(int i = 0; i < a.Length; i++){
+			if(a[i] != b[i])
+				return false;
 		}
+		return true;
 	}
 }
