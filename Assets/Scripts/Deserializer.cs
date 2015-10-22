7ac7c9381b896e75c0cd6925678fbd597184be6d
KasperHdL <kasper@honnens.dk>
2015-10-22 14:05:23 +0200

    Fixed Deserializer reading the correct

--- Assets/Scripts/Deserializer.cs
+++ Assets/Scripts/Deserializer.cs
@@ -8,37 +8,33 @@ public static class Deserializer{
 
 
     public static Vector3 Vector3(object data){
+        Vector3 v = new Vector3();
         if(data is DarkRiftReader){
-            Vector3 v;
             using(DarkRiftReader reader = (DarkRiftReader)data){
-                v   = new Vector3(
-                        reader.ReadSingle(),
-                        reader.ReadSingle(),
-                        reader.ReadSingle()
-                );
+                float[] singles = reader.ReadSingles();
+
+                for(int i = 0;i<singles.Length;i++)
+                    v[i] = singles[i];
             }
-            return v;
         }else{
             Debug.LogError("data is not a darkriftreader");
-            return new Vector3();
         }
+        return v;
     }
 
     public static Quaternion Quaternion(object data){
+        Quaternion q = new Quaternion();
         if(data is DarkRiftReader){
-            Quaternion q;
             using(DarkRiftReader reader = (DarkRiftReader)data){
-                q   = new Quaternion(
-                        reader.ReadSingle(),
-                        reader.ReadSingle(),
-                        reader.ReadSingle(),
-                        reader.ReadSingle()
-                );
-            }
-            return q;
+                float[] singles = reader.ReadSingles();
+
+                for(int i = 0;i<singles.Length;i++)
+                    q[i] = singles[i];
+
+           }
         }else{
             Debug.LogError("data is not a darkriftreader");
-            return new Quaternion();
         }
+        return q;
     }
 }
