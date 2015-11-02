06b57bbe71c4d4a4dfebd81e906abd537b0345a9
Itai Yavin <Itai.yavin@gmail.com>
2015-11-02 12:42:35 +0100

    Corrected Giant Door sound

--- Assets/Scripts/Sound/SOUND_GiantDoor.cs
+++ Assets/Scripts/Sound/SOUND_GiantDoor.cs
@@ -5,24 +5,29 @@ public class SOUND_GiantDoor : MonoBehaviour {
 	private Animator anim;
 
 	private bool isStarted;
+	private bool hasPlayed;
 	private float timeStamp;
+
 	// Use this for initialization
 	void Start () {
+		hasPlayed = false;
 		anim = GetComponent<Animator>();
 		isStarted = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (anim.GetCurrentAnimatorStateInfo(0).IsName("Open") && !isStarted)
+		if (anim.GetCurrentAnimatorStateInfo(0).IsName("Open") && !isStarted && !hasPlayed)
         {
+        	hasPlayed = true;
         	isStarted = true;
-        	SoundManager.Instance.PlayEvent("DoorSlide_Start", gameObject);
+        	SoundManager.Instance.PlayEvent("DoorSlideStart", gameObject);
         	timeStamp = Time.time + anim.GetCurrentAnimatorStateInfo(0).length;
+        	Debug.Log("timeStamp: " +timeStamp + " time: " + Time.time);
         }
 
-        if(timeStamp < Time.time && !isStarted){
-        	SoundManager.Instance.PlayEvent("DoorSlide_Stop", gameObject);
+        if(timeStamp < Time.time && isStarted){
+        	SoundManager.Instance.PlayEvent("DoorSlideStop", gameObject);
         	isStarted = false;
         }
 	
