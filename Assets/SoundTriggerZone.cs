dcb562cbb62a2f74e126662df20c2d7851936b15
Hethix <An.ryge@gmail.com>
2015-11-02 16:02:34 +0100

    Sound Crystal fixing stuff.. Totally legit fix, would not do again.

---
+++ Assets/SoundTriggerZone.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundTriggerZone : MonoBehaviour {
+
+    public SoundCrystal soundCrystal;
+
+    void OnTriggerEnter(Collider col){
+        if(col.gameObject.tag == "Player"){
+            soundCrystal.PlayerHitTriggerZone();
+            Destroy(gameObject);
+        }
+
+    }
+
+}
