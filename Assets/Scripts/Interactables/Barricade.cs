5f445cabe6a888e1851ccd1f68bde2aac28f323b
Itai Yavin <Itai.yavin@gmail.com>
2015-11-02 15:42:30 +0100

    added sound to barricade

--- Assets/Scripts/Interactables/Barricade.cs
+++ Assets/Scripts/Interactables/Barricade.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Barricade : MonoBehaviour {
 
+	private bool soundPlaying;
+
     public RailConnection rC;
     public Trigger trigger;
     public float moveSpeed = 500.0f;
@@ -18,6 +20,7 @@ public class Barricade : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		soundPlaying = false;
         t = 0;
         startPos = transform.position;
         endPos = endNode.transform.position;
@@ -29,22 +32,30 @@ public class Barricade : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		// ONLY RUNS TO POSITION NEXT SET OF BARRICADES AFTER OPENING FIRST
 		if(!isFirstBarricade){
 			if(!trigger.isTriggered && previousBarricadeHandler.isTriggered){
 				t += Time.time / moveSpeed;
 				transform.position = Vector3.Lerp (endPos, startPos, t);
+				if(!soundPlaying && !isOpened){
+					SoundManager.Instance.PlayEvent("RailwayBlockerMove", gameObject);
+					soundPlaying = true;
+				}
 			}
 		}
 
 		if(trigger.isTriggered && !isOpened){
 			t = 0;
             isOpened = true;
-            
 		}
 
         if(isOpened && t < 1){
             t += Time.time / moveSpeed;
             transform.position = Vector3.Lerp(startPos, endPos, t);
+            if(soundPlaying){
+				SoundManager.Instance.PlayEvent("RailwayBlockerMove", gameObject);
+				soundPlaying = false;
+			}
         }
 
         if(isOpened && t >= 1)
