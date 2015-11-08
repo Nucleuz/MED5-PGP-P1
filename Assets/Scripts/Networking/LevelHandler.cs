03d33cc6e733a5dfe2d1ab6890e155fc58ee6b37
Hethix <An.ryge@gmail.com>
2015-11-08 15:54:06 +0100

    Removed all sound things(we no like) and removed aesthetics from the scenes

--- Assets/Scripts/Networking/LevelHandler.cs
+++ Assets/Scripts/Networking/LevelHandler.cs
@@ -96,11 +96,12 @@ public class LevelHandler : MonoBehaviour {
             LevelManager nLM = levelContainer.levelManager;
 
             //Stich togehter with the levelContainers and set the next as current..
-            Vector3 pNLD = pLM.nextLevelDirection.normalized;
+            Vector3 pLD = pLM.levelEndRail[0].transform.position - pLM.levelEndRail[0].next.transform.position;
+            Vector3 nLD = nLM.levelStartRail[0].transform.position - nLM.levelStartRail[0].next.transform.position;
         
-            //rotate next level so that pLM.nextLevelDirection is equal to the inverse nLM.prevLevelDirection
-            Console.Instance.AddMessage(pLM.nextLevelDirection + " " + (Mathf.Rad2Deg * levelContainers[loadingIndex - 1].transform.rotation.y ));
-            float a = Vector3.Angle(pLM.nextLevelDirection,nLM.prevLevelDirection) - currentRotation;
+            //rotate next level so that pLD is equal to the inverse nLD
+            Console.Instance.AddMessage(pLD + " " + (Mathf.Rad2Deg * levelContainers[loadingIndex - 1].transform.rotation.y ));
+            float a = Vector3.Angle(pLD,nLD) - currentRotation;
             currentRotation = 180-a;
 
             //rotate new level
@@ -108,7 +109,7 @@ public class LevelHandler : MonoBehaviour {
 
             //levelOffset is the amount of space between levels @TODO should be something meaningful
             float levelOffset = 2;
-            Vector3 nLevelRailPos = pLM.levelEndRail[0].transform.position + pNLD * levelOffset;
+            Vector3 nLevelRailPos = pLM.levelEndRail[0].transform.position + pLD * levelOffset;
             Vector3 delta = nLevelRailPos - nLM.levelStartRail[0].transform.position;
             
             //offset the next level so that it is positioned correctly 
