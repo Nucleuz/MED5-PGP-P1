5c3d91f077d1d70943f8770fe4c9e5df5be9901b
Frans Larsen <larsen.frans@gmail.com>
2015-11-09 09:48:44 +0100

    Added some server info for debugging (Not much yet)

---
+++ Assets/ServerInfo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using DarkRift;
+
+public class ServerInfo : MonoBehaviour {
+	private Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+	}
+
+	void OnGUI() {
+		string output = "";
+		output += "Clients: " + DarkRiftServer.GetNumberOfConnections();
+		output += "\ntotalExecutionCounts: " + PerformanceMonitor.totalExecutionCounts;
+		output += "\ntotalExecutionTime: " + PerformanceMonitor.totalExecutionTime;
+		output += "\naverageExecutionTime: " + PerformanceMonitor.averageExecutionTime;
+		text.text = output;
+	}
+}
