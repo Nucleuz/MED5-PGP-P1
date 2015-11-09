9fec1fec801a47093709973e2cd2e318ddf07ea2
Itai Yavin <Itai.yavin@gmail.com>
2015-11-09 13:27:57 +0100

    Lava sounds implemented

---
+++ Assets/AMB_Lava.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AMB_Lava : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SoundManager.Instance.PlayEvent("Lava_Active", gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
