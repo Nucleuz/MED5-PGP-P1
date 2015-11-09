51718eca59a7c9aa30d27009f8fa28b6a1b46c35
Itai Yavin <Itai.yavin@gmail.com>
2015-11-09 15:04:47 +0100

    Fixed some barricade sounds and other shit.

---
+++ Assets/Scripts/Sound/SOUND_CarCrystal.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SOUND_CarCrystal : MonoBehaviour {
+
+	public Cart car;
+	public bool isPlaying;
+
+	// Use this for initialization
+	void Start () {
+		isPlaying = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(car.isMoving){
+			SoundManager.Instance.SetRTPC("CarSpeed", Input.GetAxis("Vertical")*10, gameObject);
+			if(!isPlaying){
+				SoundManager.Instance.ToggleSwitch("CarOn_Off", "On", gameObject);
+				SoundManager.Instance.PlayEvent("CarCrystal", gameObject);
+				isPlaying = true;
+			}
+		}
+		if(!car.isMoving && isPlaying){
+			SoundManager.Instance.StopEvent("CarCrystal", gameObject);
+			SoundManager.Instance.ToggleSwitch("CarOn_Off", "Off", gameObject);
+			SoundManager.Instance.PlayEvent("CarCrystal", gameObject);
+			isPlaying = false;
+		}
+
+	}
+}
---
+++ Assets/Scripts/Sound/SOUND_RailwayBlockerCrystal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SOUND_RailwayBlockerCrystal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SoundManager.Instance.PlayEvent("RailwayBlockerCrystal", gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
