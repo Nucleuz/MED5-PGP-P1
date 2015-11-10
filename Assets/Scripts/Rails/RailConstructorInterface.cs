23e98bb1706467a5d7798df467bad5c52214f4e2
Hethix <An.ryge@gmail.com>
2015-11-10 17:28:23 +0100

    Fixed compiling for interface

--- Assets/Scripts/Rails/RailConstructorInterface.cs
+++ Assets/Scripts/Rails/RailConstructorInterface.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
 using UnityEditor;
 using System.Collections;
 
@@ -41,3 +42,4 @@ public class RailConstructorInterface : EditorWindow {
 	}
 
 }
+#endif
