09ba598760f3957bc200d789c1bb6083a9365455
Maja Daniela Hansen <MajaDanielaHansen@Gmail.com>
2015-11-12 20:33:05 +0100

    Created a timer for the game. This holds the possibility to handle each level play time and overall play time. Must be connected with the server.
    The file name is the current time.

--- Assets/Scenes/Networking/TestTimer.cs
+++ Assets/Scenes/Networking/TestTimer.cs
@@ -1,36 +1,73 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.IO;
+using System;
 
 public class TestTimer : MonoBehaviour {
-
-	//public string savePath;
 	private string fileName;
-	private int fileNameNumber;
+	public String currentDate;
+	int levelIndex;
+	float lvl1, lvl2, lvl3, lvl4, lvl5, lvl6;
+
 
 	// Use this for initialization
 	void Start () {
-		fileNameNumber = 0;
-		fileName = "Test" + fileNameNumber.ToString();
-		if(File.Exists(fileName)){
-			Debug.Log("The filename " + fileName + " already exists. Creating a new file with 1 added to its number");
-			fileNameNumber++;
-			fileName = "Test" + fileNameNumber.ToString();	
-		}
-
-		// Compose a string that consists of three lines.
-		string lines = "First line.\r\nSecond line.\r\nThird line.";
-		
-		// Write the string to a file.
-		System.IO.StreamWriter file = new System.IO.StreamWriter();
-		file.WriteLine(lines);
-		
-		file.Close();
-
+		//Setting the name for the file to the current time(hour and minute)
+		currentDate = System.DateTime.Now.ToString("HHmm");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		saveTime(); //Only call then the game ends.
+		checkLevel();
+		if(Input.GetKeyDown(KeyCode.F)){ //This should be handled by gameManager.
+			levelIndex+=1;
+		}
 	}
+
+	public void checkLevel(){
+		switch (levelIndex){
+            case 1:
+            	lvl1+=Time.deltaTime;
+            break;
+            case 2:
+            	lvl2+=Time.deltaTime; 	
+            break;
+            case 3:
+            	lvl3+=Time.deltaTime;
+            break;
+
+			case 4:
+            	lvl4+=Time.deltaTime;
+            break;
+            case 5:
+            	lvl5+=Time.deltaTime;
+            break;
+            case 6:
+            	lvl6+=Time.deltaTime;           	
+            break;
+
+            default:
+                Debug.Log("Invalid levelIndex");
+            break;
+        }  
+	}
+
+	public void saveTime(){
+		float overallTime = lvl1 + lvl2 +lvl3 +lvl4 +lvl5 +lvl6;
+		//Saving the times for all the puzzle
+		string times = 
+		     "Timer puzzle#1: " + lvl1 + 
+		".\r\nTimer puzzle#2: " + lvl2 +
+		".\r\nTimer puzzle#3: " + lvl3 +
+		".\r\nTimer puzzle#4: " + lvl4 +
+		".\r\nTimer puzzle#5: " + lvl5 +
+		".\r\nTimer puzzle#6: " + lvl6 +
+		".\r\nTimer overall: " + overallTime;
+		//Creating a file
+		System.IO.StreamWriter file = new System.IO.StreamWriter(currentDate);
+		//Write the data(times) in a file.
+		file.WriteLine(times);
+		file.Close();
+	}	
 }
