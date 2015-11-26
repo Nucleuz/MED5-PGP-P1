4906a7ca74e611b6a71caedf84943b3b8622a9c8
Hethix <An.ryge@gmail.com>
2015-11-26 10:41:55 +0100

    Added a prefab and script so we can control the fog based on where the players are in the game.

---
+++ Assets/Scripts/TriggerZones/FogTriggerZone.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogTriggerZone : MonoBehaviour {
+	public bool fadeDensity;
+	[Space(-2)]
+	public float fadeDensityTo;
+	[Space(10)]
+	public bool fadeColor;
+	[Space(-2)]
+	public Color fadeColorTo;
+	[Space(10)]
+	public float fadeLength;
+
+
+	void OnTriggerEnter(Collider col){
+		if(fadeDensity){
+			FogControl.Instance.FadeDensityTo(fadeDensityTo, fadeLength);
+		}
+		if(fadeColor){
+			FogControl.Instance.FadeColorTo(fadeColorTo, fadeLength);
+		}
+        Destroy(gameObject);
+    }
+}
