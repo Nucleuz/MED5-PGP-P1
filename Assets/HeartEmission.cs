4246f8e26bc78892ab4853e08591f07de05e9f71
Nucleuz <h-hansen1990@hotmail.com>
2015-11-26 15:22:15 +0100

    Added a HeatEmission script that should be used in the last puzzle.

---
+++ Assets/HeartEmission.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeartEmission : MonoBehaviour {
+
+	Trigger trigger;
+
+	Renderer renderer;
+	Material material;
+	Material matInstance;
+
+	float lerpColor;
+	float t;
+	float lerpSpeed;
+	
+	void Start()
+	{
+		trigger = GetComponent<Trigger> ();
+
+		renderer = GetComponent<Renderer>();
+		material = renderer.sharedMaterial;
+		matInstance = Instantiate (material);
+		t = 0f;
+		lerpSpeed = 0.15f;
+
+
+	}
+	
+	void Update()
+	{  
+		if (trigger.isTriggered) {
+			if (t < 1f) {
+				Debug.Log(t);
+				t += Time.deltaTime * lerpSpeed;
+				lerpColor = Mathf.Lerp (0f, 1f, t);
+				matInstance.EnableKeyword ("_EMISSION");
+				matInstance.SetColor ("_EmissionColor", new Color (lerpColor, lerpColor, lerpColor));
+			}
+
+			// Start to fade the screen to white when the heart reaches 0.5 emission
+			if(t > 0.5f){
+				CanvasFade.Instance.ToWhite (0.5f);
+			}
+		}
+	}
+}
